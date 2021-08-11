9485817a054d537f984d1563cc380e20914e2cfe
Crowdbotics <team@crowdbotics.com>
2021-08-11 04:50:23 +0000

    Initial commit.

---
+++ backend/club_control_app_29711/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for club_control_app_29711 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'club_control_app_29711.settings')
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "club_control_app_29711Identifier",
+  url: "https://club_control_app_29711Identifier.botics.co",
+  api: "https://club_control_app_29711Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
